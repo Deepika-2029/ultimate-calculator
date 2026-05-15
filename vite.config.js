1fa7296bc21a0a451b3d828347c38f3aaae1f26a
DJ <jeenandra.2003@gmail.com>
2026-05-15 07:54:47 +0530

    Remove base path for Netlify hosting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages ke liye base URL zaroori hai
-  base: "/ultimate-calculator/",
 });
